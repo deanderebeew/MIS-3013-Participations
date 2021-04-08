4fb2f98c8e09b98c3bd78b34ff8f3afc3fbec033
Dean Derebeew <deanderebeew@ou.edu>
2021-04-08 15:35:30 -0500

    holy cow I actually did it. Proud

--- Participations/FunctionStuffLists/Program.cs
+++ Participations/FunctionStuffLists/Program.cs
@@ -81,7 +81,65 @@ namespace FunctionStuffLists
             //        return false;
             //    }
 
+            //3 and 4 are arguments 
+            //double sum = Add(3, 4);
 
+
+
+            //Calling a method for some Letter grade action!
+
+
+            //string answer;
+            //do
+            //{
+            //    Console.WriteLine("Please input your grade in the course");
+            //    double grade = Convert.ToDouble(Console.ReadLine());
+
+            //    string lg = ConvertGradeToLetterGrade(grade); //can also put a double value here as long as its a double doesnt have to be avariable
+
+            //    Console.WriteLine($"Congratulations you have an {lg} in the course");
+
+            //    Console.WriteLine("Do you want to convert another grade? Yes or No");
+            //    answer = Console.ReadLine();
+            //} while (answer.ToLower()[0] == 'y');
+
+            /// <summary>
+            /// Converts a standard grade (98.5) to the appropriate letter grade (A, B, C)
+            /// </summary>
+            /// <param name="anyVariableNameWeWantThisBelongsToThisMethod">The grade to convert to a letter grade </param>
+            /// <returns>A string that is the letter grade of the input. N/A if it is below 0 </returns>
+
+            //static string ConvertGradeToLetterGrade(double anyVariableNameWeWantThisBelongsToThisMethod)
+            //{
+            //    string letterGrade;
+            //    if (anyVariableNameWeWantThisBelongsToThisMethod >= 90)
+            //    {
+            //        letterGrade = "A";
+            //    }
+            //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 80)
+            //    {
+            //        letterGrade = "B";
+            //    }
+            //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 70)
+            //    {
+            //        letterGrade = "C";
+            //    }
+            //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 60)
+            //    {
+            //        letterGrade = "D";
+            //    }
+            //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 0)
+            //    {
+            //        letterGrade = "F";
+            //    }
+            //    else
+            //    {
+            //        letterGrade = "N/A";
+            //    }
+
+
+            //    return letterGrade;
+            //}
 
             Console.WriteLine("Hello World!");
         }
--- Participations/Functions/Program.cs
+++ Participations/Functions/Program.cs
@@ -13,75 +13,134 @@ namespace Functions
             DeveloperName("Dean", "MIS", "April 5th");
 
             double result = 0;
-
+            string answer2 = "n";
+           
 
             do
             {
-                Console.WriteLine("What type of calculation do you wish to perform? >>");
+
+               
+
+                
+                Console.WriteLine("What type of calculation do you wish to perform? 1 for Addition, 2 for Subtraction, 3 for Multiplication, and 4 for Division >>");
                 string calculation = Console.ReadLine();
 
-                if (calculation.ToLower() == "Addition")
+                if (calculation.ToLower() == "1")
                 {
-                    Console.WriteLine("What is the first number of the addition? >>");
-                    string firstnumber = Console.ReadLine();
-                    double firstdouble = Convert.ToDouble(firstnumber);
+                    double firstdouble;
+                    double seconddouble;
+                    
+
+                    if (answer2.ToLower() == "1")
+                    {
+                        firstdouble = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("What is the first number of the addition? >>");
+                        string firstnumber = Console.ReadLine();
+                        firstdouble = Convert.ToDouble(firstnumber);
+                    }
+                   
+
 
                     Console.WriteLine("What is the second number of the addition? >>");
                     string secondnumber = Console.ReadLine();
-                    double seconddouble = Convert.ToDouble(secondnumber);
+                    seconddouble = Convert.ToDouble(secondnumber);
 
                    result = Add(firstdouble, seconddouble);
 
                     Console.WriteLine($"The result of your calculation is {result}");
 
+                    
+
                 }
 
-                else if (calculation.ToLower() == "Subtraction")
+                else if (calculation.ToLower() == "2")
                 {
-                    Console.WriteLine("What is the first number of the subtraction? >>");
-                    string firstnumber = Console.ReadLine();
-                    double firstdouble = Convert.ToDouble(firstnumber);
+                    double firstdouble;
+                    double seconddouble;
+
+
+                    if (answer2.ToLower() == "1")
+                    {
+                        firstdouble = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("What is the first number of the subtraction? >>");
+                        string firstnumber = Console.ReadLine();
+                        firstdouble = Convert.ToDouble(firstnumber);
+                    }
 
                     Console.WriteLine("What is the second number of the subtraction? >>");
                     string secondnumber = Console.ReadLine();
-                    double seconddouble = Convert.ToDouble(secondnumber);
+                    seconddouble = Convert.ToDouble(secondnumber);
 
                     result = Subtract(firstdouble, seconddouble);
 
                     Console.WriteLine($"The result of your calculation is {result}");
 
+                   
+
                 }
 
-                else if (calculation.ToLower() == "Multiplication")
+                else if (calculation.ToLower() == "3")
                 {
-                    Console.WriteLine("What is the first number of the multplication? >>");
-                    string firstnumber = Console.ReadLine();
-                    double firstdouble = Convert.ToDouble(firstnumber);
+                    double firstdouble;
+                    double seconddouble;
+
+
+                    if (answer2.ToLower() == "1")
+                    {
+                        firstdouble = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("What is the first number of the multiplication? >>");
+                        string firstnumber = Console.ReadLine();
+                        firstdouble = Convert.ToDouble(firstnumber);
+                    }
 
                     Console.WriteLine("What is the second number of the multiplication? >>");
                     string secondnumber = Console.ReadLine();
-                    double seconddouble = Convert.ToDouble(secondnumber);
+                    seconddouble = Convert.ToDouble(secondnumber);
 
                     result = Multiply(firstdouble, seconddouble);
 
                     Console.WriteLine($"The result of your calculation is {result}");
 
+                    
+
                 }
 
-               else if (calculation.ToLower() == "Division")
+               else if (calculation.ToLower() == "4")
                 {
-                    Console.WriteLine("What is the first number of the division? >>");
-                    string firstnumber = Console.ReadLine();
-                    double firstdouble = Convert.ToDouble(firstnumber);
+                    double firstdouble;
+                    double seconddouble;
+
+
+                    if (answer2.ToLower() == "1")
+                    {
+                        firstdouble = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("What is the first number of the division? >>");
+                        string firstnumber = Console.ReadLine();
+                        firstdouble = Convert.ToDouble(firstnumber);
+                    }
 
                     Console.WriteLine("What is the second number of the division? >>");
                     string secondnumber = Console.ReadLine();
-                    double seconddouble = Convert.ToDouble(secondnumber);
+                    seconddouble = Convert.ToDouble(secondnumber);
 
                     result = Divide(firstdouble, seconddouble);
 
                     Console.WriteLine($"The result of your calculation is {result}");
 
+
+                   
 
                 }
 
@@ -90,85 +149,22 @@ namespace Functions
                 answer = Console.ReadLine();
 
 
-
-                
-
-                if (answer.ToLower() == "N")
+                if (answer.ToLower() == "n")
                 {
 
-
-
-                    Console.WriteLine("Would you like to use the result of the previous calculation as the first value in a follow-up calculation? Y or N");
-                    string answer2 = Console.ReadLine();
-
-
-
-
+                    Console.WriteLine("Would you like to use the result of the previous calculation as the first value in a follow-up calculation? 1 for Y or 2 for N");
+                    answer2 = Console.ReadLine();
                 }
+                
+
+
 
 
             } while (answer.ToLower()[0] != 'y');
 
-            //3 and 4 are arguments 
-            //double sum = Add(3, 4);
-
             
 
-            //Calling a method for some Letter grade action!
-
-
-            //string answer;
-            //do
-            //{
-            //    Console.WriteLine("Please input your grade in the course");
-            //    double grade = Convert.ToDouble(Console.ReadLine());
-
-            //    string lg = ConvertGradeToLetterGrade(grade); //can also put a double value here as long as its a double doesnt have to be avariable
-
-            //    Console.WriteLine($"Congratulations you have an {lg} in the course");
-
-            //    Console.WriteLine("Do you want to convert another grade? Yes or No");
-            //    answer = Console.ReadLine();
-            //} while (answer.ToLower()[0] == 'y');
-
         }
-        /// <summary>
-        /// Converts a standard grade (98.5) to the appropriate letter grade (A, B, C)
-        /// </summary>
-        /// <param name="anyVariableNameWeWantThisBelongsToThisMethod">The grade to convert to a letter grade </param>
-        /// <returns>A string that is the letter grade of the input. N/A if it is below 0 </returns>
-
-        //static string ConvertGradeToLetterGrade(double anyVariableNameWeWantThisBelongsToThisMethod)
-        //{
-        //    string letterGrade;
-        //    if (anyVariableNameWeWantThisBelongsToThisMethod >= 90)
-        //    {
-        //        letterGrade = "A";
-        //    }
-        //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 80)
-        //    {
-        //        letterGrade = "B";
-        //    }
-        //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 70)
-        //    {
-        //        letterGrade = "C";
-        //    }
-        //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 60)
-        //    {
-        //        letterGrade = "D";
-        //    }
-        //    else if (anyVariableNameWeWantThisBelongsToThisMethod >= 0)
-        //    {
-        //        letterGrade = "F";
-        //    }
-        //    else
-        //    {
-        //        letterGrade = "N/A";
-        //    }
-
-
-        //    return letterGrade;
-        //}
 
         static void DeveloperName(string Name, string Class, string Date)
         {
